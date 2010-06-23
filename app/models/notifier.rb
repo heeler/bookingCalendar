c2355a5ba9fc6099e65439b93114da58ca9b2421
Jamie Sherman <heeler@mac.com>
2010-06-23 16:59:13 -0700

    changed the email account to a facility one

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -2,28 +2,28 @@ class Notifier < ActionMailer::Base
   
   def booking_canceled(booking)
     recipients  booking.user.email
-    from        "jamie.sherman@gmail.com"
+    from        "msfCalendar@gmail.com"
     subject     "RE: You're #{booking.instrument.name} booking canceled"
     body        :event => booking
   end
   
   def instrument_delay(booking)
     recipients  booking.user.email
-    from        "jamie.sherman@gmail.com"
+    from        "msfCalendar@gmail.com"
     subject     "RE: You're #{booking.instrument.name} booking delayed"
     body        :event => booking
   end
 
   def booking_confirmation(booking)
     recipients  booking.user.email
-    from        "jamie.sherman@gmail.com"
+    from        "msfCalendar@gmail.com"
     subject     "RE: You're #{booking.instrument.name} booking details"
     body        :event => booking
   end
 
   def booking_modification(booking)
     recipients  booking.user.email
-    from        "jamie.sherman@gmail.com"
+    from        "msfCalendar@gmail.com"
     subject     "RE: You're #{booking.instrument.name} booking has been modified."
     body        :event => booking
   end
