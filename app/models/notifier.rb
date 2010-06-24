79a6e0e95f33b5963f18127ab2ccf7db9cfe1e4e
Jamie Sherman <heeler@mac.com>
2010-06-24 13:29:19 -0700

    corrected the spelling in email notifications

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -3,28 +3,28 @@ class Notifier < ActionMailer::Base
   def booking_canceled(booking)
     recipients  booking.user.email
     from        "msfCalendar@gmail.com"
-    subject     "RE: You're #{booking.instrument.name} booking canceled"
+    subject     "RE: Your #{booking.instrument.name} booking canceled"
     body        :event => booking
   end
   
   def instrument_delay(booking)
     recipients  booking.user.email
     from        "msfCalendar@gmail.com"
-    subject     "RE: You're #{booking.instrument.name} booking delayed"
+    subject     "RE: Your #{booking.instrument.name} booking delayed"
     body        :event => booking
   end
 
   def booking_confirmation(booking)
     recipients  booking.user.email
     from        "msfCalendar@gmail.com"
-    subject     "RE: You're #{booking.instrument.name} booking details"
+    subject     "RE: Your #{booking.instrument.name} booking details"
     body        :event => booking
   end
 
   def booking_modification(booking)
     recipients  booking.user.email
     from        "msfCalendar@gmail.com"
-    subject     "RE: You're #{booking.instrument.name} booking has been modified."
+    subject     "RE: Your #{booking.instrument.name} booking has been modified."
     body        :event => booking
   end
 
