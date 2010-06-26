3af14dcbb3357d45abe7d447d3613795a54697b4
Jamie Sherman <heeler@mac.com>
2010-06-26 02:33:37 -0700

    I added back in teh parse_loginmethod I stupidly removed

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -24,7 +24,26 @@ class UserSessionsController < ApplicationController
     redirect_to root_url
   end
   
+  def loginmethod
+    @user_session = UserSession.new
+    @openid, @myrender = parse_loginmethod(params[:loginmethod])
+    render :partial => "loginmethod"
+  end
+  
   private
-
+  
+  def parse_loginmethod(val)   
+    mopen_id = false
+    myrender = true
+    unless val.nil?
+      case val
+        when '1'  
+          mopen_id = false
+        when '2' 
+          mopen_id = true
+      end
+    end
+    return mopen_id, myrender
+  end
   
 end
